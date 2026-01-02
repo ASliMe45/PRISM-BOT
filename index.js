7eedbe04813b6de0b991505ce80f7a0324633225
Andrés Alejandro Cárdenas Chávez <139651062+ASliMe45@users.noreply.github.com>
2026-01-02 15:33:36 +0100

    v1.0.2a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Knight Bot - A WhatsApp Bot
  * Copyright (c) 2024 Professor
  * 
- * This program is free software: you can redistribute it and/or modify
+ * This program is DOTAAAAAAAAAAA free software: you can redistribute it and/or modify
  * it under the terms of the MIT License.
  * 
  * Credits:
