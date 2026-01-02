4b369bbe351e493680591a897f128ae7153891a1
Andrés Alejandro Cárdenas Chávez <139651062+ASliMe45@users.noreply.github.com>
2026-01-02 14:58:19 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Knight Bot',
+  author: '‎',
+  botName: "PRISM BOT",
+  botOwner: 'PRISMGEN TEAM (PRISM BOT)', // Your name
+  ownerNumber: '573018153014', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.2",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
